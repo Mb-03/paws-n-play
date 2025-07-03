138e011a8813270f0e54c5e25620b056d0e2aff9
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-07-03 14:54:52 +0400

    feature/swiper

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  loop: true,
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 24,
+    },
+
+    // when window width is >= 643px
+    643: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+
+    // when window width is >= 907px
+    907: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    // when window width is >= 640px
+    1185: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
   },
 });
 
